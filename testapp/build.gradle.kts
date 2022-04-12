a8eb91e212d492d7a124ee15bd7876e9b8da8764
Jarle Hansen <Jarle.Hansen@skatteetaten.no>
2022-04-12 11:34:50 +0200

    updated deps

--- testapp/build.gradle.kts
+++ testapp/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("no.skatteetaten.gradle.aurora") version "4.4.12"
-    id("org.springframework.boot") version "2.6.5"
+    id("no.skatteetaten.gradle.aurora") version "4.4.15"
+    id("org.springframework.boot") version "2.6.6"
 }
 
 aurora {
@@ -23,10 +23,10 @@ dependencies {
     implementation(project(":aurora-spring-boot-webflux-starter"))
 
     implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.6.10")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.6.10")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.6.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j:1.6.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.6.20")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.6.20")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.6.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j:1.6.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
     implementation("io.github.microutils:kotlin-logging-jvm:2.1.21")
 }
