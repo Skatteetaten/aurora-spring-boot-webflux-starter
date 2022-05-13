97368d9aa39b6173949277b8bbe684e4419db526
Jarle Hansen <Jarle.Hansen@skatteetaten.no>
2022-05-13 12:56:59 +0200

    updated to use gradle

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,18 @@
+object Versions {
+    const val auroraBaseStarter = "1.3.7"
+
+    const val kotlin = "1.6.21"
+    const val kotlinCoroutines = "1.6.1"
+    const val kotlinLogging = "2.1.21"
+    const val springCloud = "2021.0.2"
+
+    const val assertk = "0.25"
+    const val mockwebserver = "4.9.3"
+    const val testcontainers = "1.16.3"
+    const val awaitility = "4.2.0"
+}
+
+object PluginVersions {
+    const val aurora = "4.4.17"
+    const val springBoot = "2.6.7"
+}
