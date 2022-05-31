afe6121f47f0167316c4567321f0be09fcba7348
Jarle Hansen <Jarle.Hansen@skatteetaten.no>
2022-05-31 14:11:22 +0200

    bump deps

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -2,14 +2,14 @@ object Versions {
     const val auroraBaseStarter = "1.3.12"
 
     const val kotlin = "1.6.21"
-    const val kotlinCoroutines = "1.6.1"
+    const val kotlinCoroutines = "1.6.2"
     const val kotlinLogging = "2.1.21"
     const val springCloud = "2021.0.2"
 
     const val assertk = "0.25"
     const val mockwebserver = "3.14.9"
     const val awaitility = "4.2.0"
-    const val junitPioneer = "1.7.0"
+    const val junitPioneer = "1.7.1"
 }
 
 object PluginVersions {
