b6e427276d0153eaa0b1d2d7d375b9c3e5ceecb1
Jarle Hansen <Jarle.Hansen@skatteetaten.no>
2022-05-31 14:00:18 +0200

    added basic auth on zipkin webclient

--- aurora-spring-boot-webflux-starter/src/main/java/no/skatteetaten/aurora/webflux/config/WebFluxStarterApplicationConfig.java
+++ aurora-spring-boot-webflux-starter/src/main/java/no/skatteetaten/aurora/webflux/config/WebFluxStarterApplicationConfig.java
@@ -1,12 +1,18 @@
 package no.skatteetaten.aurora.webflux.config;
 
+import java.nio.charset.StandardCharsets;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.reactive.function.client.WebClientCustomizer;
 import org.springframework.cloud.sleuth.instrument.web.HttpServerRequestParser;
+import org.springframework.cloud.sleuth.zipkin2.ZipkinWebClientBuilderProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.util.Base64Utils;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import brave.http.HttpRequestParser;
 import no.skatteetaten.aurora.webflux.AuroraRequestParser;
@@ -23,8 +29,8 @@ public class WebFluxStarterApplicationConfig {
         @Value("${app.version:}") String appVersion,
         @Value("${aurora.klientid:}") String klientIdEnv
     ) {
-        String fallbackKlientId = !appVersion.isBlank() ? String.format("%s/%s", appName, appVersion) : appName;
-        String klientId = !klientIdEnv.isBlank() ? klientIdEnv : fallbackKlientId;
+        String fallbackKlientId = appVersion.isBlank() ? appName : String.format("%s/%s", appName, appVersion);
+        String klientId = klientIdEnv.isBlank() ? fallbackKlientId : klientIdEnv;
         return new AuroraWebClientCustomizer(klientId);
     }
 
@@ -38,5 +44,19 @@ public class WebFluxStarterApplicationConfig {
     @ConditionalOnProperty(prefix = "spring.zipkin", name = "enabled", havingValue = "false", matchIfMissing = true)
     public AuroraSpanHandler auroraSpanHandler() {
         return new AuroraSpanHandler();
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "aurora.webflux.trace.auth", name = { "username", "password" })
+    public ZipkinWebClientBuilderProvider zipkinWebClientBuilderProvider(
+        @Value("aurora.webflux.trace.auth.username") String username,
+        @Value("aurora.webflux.trace.auth.password") String password,
+        WebClient.Builder builder
+    ) {
+        return () -> {
+            String encodedAuth =
+                Base64Utils.encodeToString((username + ":" + password).getBytes(StandardCharsets.UTF_8));
+            return builder.defaultHeader(HttpHeaders.AUTHORIZATION, "Basic " + encodedAuth);
+        };
     }
 }
