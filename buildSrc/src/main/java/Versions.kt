dc644d7e3b12caf6f2da4bccfd1cb532f62db2f3
Jørgen Bugge <70197376+bugge-jorgen@users.noreply.github.com>
2022-11-03 12:08:53 +0100

    AOS-7199: upgraded to spring boot 2.7.5, aurora-gradle-plugin 4.5.9, aurora-spring-boot-base-starter 1.4.4 (#41)

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,18 +1,18 @@
 object Versions {
-    const val auroraBaseStarter = "1.4.3"
+    const val auroraBaseStarter = "1.4.4"
 
-    const val kotlin = "1.7.10"
+    const val kotlin = "1.7.20"
     const val kotlinCoroutines = "1.6.4"
-    const val kotlinLogging = "2.1.23"
-    const val springCloud = "2021.0.3"
+    const val kotlinLogging = "3.0.4"
+    const val springCloud = "2021.0.4"
 
     const val assertk = "0.25"
-    const val mockwebserver = "4.9.3"
+    const val mockwebserver = "4.10.0"
     const val awaitility = "4.2.0"
     const val junitPioneer = "1.7.1"
 }
 
 object PluginVersions {
-    const val aurora = "4.5.6"
-    const val springBoot = "2.7.3"
+    const val aurora = "4.5.9"
+    const val springBoot = "2.7.5"
 }
